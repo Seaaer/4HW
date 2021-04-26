3fa7c4324b3f3448ae39fa3c88fad7b08a886624
帛勳 謝 <s881230x@gmail.com>
2021-04-26 13:45:22 +0800

    homework4

--- 4HW/DropdownListShow.aspx
+++ 4HW/DropdownListShow.aspx
@@ -10,6 +10,7 @@
 <body>
     <form id="form1" runat="server">
         <div>
+            <asp:DropDownList ID="DropDownList1" runat="server"></asp:DropDownList>
         </div>
     </form>
 </body>
--- 4HW/DropdownListShow.aspx.designer.cs
+++ 4HW/DropdownListShow.aspx.designer.cs
@@ -7,10 +7,12 @@
 // </自動產生的>
 //------------------------------------------------------------------------------
 
-namespace _4HW {
+namespace _4HW
+{
 
 
-    public partial class DropdownListShow {
+    public partial class DropdownListShow
+    {
 
         /// <summary>
         /// form1 控制項。
@@ -20,5 +22,14 @@ namespace _4HW {
         /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// DropDownList1 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
     }
 }
